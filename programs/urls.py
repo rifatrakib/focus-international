f22ae7194d1998a5d35b280a578fa77fc390b841
rifatrakib <Rakib.1508@outlook.com>
2022-05-14 07:16:30 +0600

    program details page connected with program list page

--- programs/urls.py
+++ programs/urls.py
@@ -3,5 +3,6 @@ from programs import views
 
 app_name = 'programs'
 urlpatterns = [
-    path('', views.get_programs, name='programs-summary'),
+    path('', views.get_programs_summary, name='programs-summary'),
+    path('<int:program_id>/', views.get_program_details, name='program-details'),
 ]
--- programs/views.py
+++ programs/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from programs.models import Program
 
 
-def get_programs(request):
+def get_programs_summary(request):
     programs = Program.objects.all()
     context = {'programs': programs}
     return render(request, 'programs/programs-summary.html', context)
+
+
+def get_program_details(request, program_id):
+    program = Program.objects.get(pk=program_id)
+    context = {'program': program}
+    return render(request, 'programs/program-details.html', context)

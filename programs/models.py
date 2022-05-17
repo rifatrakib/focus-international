18054f5dd3a0d82b7cea3583b44fd2d91aef29af
rifatrakib <Rakib.1508@outlook.com>
2022-05-17 15:58:59 +0600

    array fields removed from program model for simplicity

--- programs/models.py
+++ programs/models.py
@@ -10,15 +10,15 @@ class Program(models.Model):
         MIXED = 'mixed', _('Mixed')
     
     name = models.CharField(_('Title of Program'), max_length=50, unique=True)
-    tagline = models.CharField(_('Sub-title of Program'), max_length=150, blank=True, null=True)
+    tagline = models.CharField(_('Sub-title of Program'), max_length=100, blank=True, null=True)
     deadline = models.DateField(_('Application Deadline'), blank=True, null=True)
     introduction = models.CharField(_('Program Introduction'), max_length=250, blank=True, null=True)
-    teaching_mode = models.CharField(_('Teaching Mode'), max_length=10, blank=True, null=True)
-    academic_history = ArrayField(models.CharField(_('Academic History'), max_length=25), blank=True, null=True)
-    advantages = ArrayField(models.CharField(_('Program Advantages'), max_length=50), blank=True, null=True)
-    description = models.CharField(_('General Description'), max_length=1000, blank=True, null=True)
-    scholarships = models.CharField(_('Scholarship Infomation'), max_length=100, blank=True, null=True)
-    institutions = ArrayField(models.CharField(_('Institutes'), max_length=50), blank=True, null=True)
+    teaching_mode = models.CharField(_('Teaching Mode'), max_length=10, choices=TeachingModeChoices.choices, blank=True, null=True)
+    academic_history = models.CharField(_('Academic History'), max_length=100, blank=True, null=True)
+    advantages = models.CharField(_('Program Advantages'), max_length=500, blank=True, null=True)
+    description = models.CharField(_('General Description'), max_length=2000, blank=True, null=True)
+    scholarships = models.CharField(_('Scholarship Infomation'), max_length=250, blank=True, null=True)
+    institutions = models.CharField(_('Institutes'), max_length=500, blank=True, null=True)
     extra_information = models.JSONField(_('Additional information'), null=True, blank=True)
     
     class Meta:
